7ddcf8c4068598cb892c3a7175b8661fdb9659ec
pepe tellez <mcbelme92@gmail.com>
2025-07-10 21:49:17 -0600

    feat:sonar-project.properties add

--- src/__tests__/TaskItem.test.jsx
+++ src/__tests__/TaskItem.test.jsx
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import TaskItem from "../components/TaskItem";
+import TaskInput from "../components/TaskInput";
 
 describe("TaskItem", () => {
   const task = { text: "Aprender Jest", completed: false };
@@ -41,5 +42,13 @@ describe("TaskItem", () => {
   render(<TaskItem task={task} />);
   // Espera que no aparezca en el documento
   expect(screen.queryByText(longText)).not.toBeInTheDocument();
+ });
+  test("agrega una tarea al hacer click", () => {
+  render(<TaskInput />);
+  const input = screen.getByPlaceholderText(/nueva tarea/i);
+  fireEvent.change(input, { target: { value: "Probar coverage" } });
+  const button = screen.getByText(/agregar/i);
+  fireEvent.click(button);
+  // Aquí podrías esperar a que se llame addTask, dependiendo de cómo mockees el store
 });
 });
